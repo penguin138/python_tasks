1144d202531c2079ac8596f75ecaf8b60cabf793
penguin138 <fridaythethirteens@gmail.com>
2016-05-21 08:41:42 +0300

    add docstrings to methods

--- home_work_5/game_of_life/main.py
+++ home_work_5/game_of_life/main.py
@@ -19,14 +19,17 @@ class LifeGame(BoxLayout):
         self.updates_called = 1
 
         def on_press_pause(instance):
+            """Callback for pause button"""
             root = instance.parent.parent
             root.pause_pressed = True
 
         def on_press_start(instance):
+            """Callback for start button"""
             root = instance.parent.parent
             root.pause_pressed = False
 
         def on_press_field_cell(instance):
+            """Callback for field cell button"""
             root = instance.parent.parent
             i, j = instance.coordinates
             if root.pause_pressed:
@@ -37,44 +40,34 @@ class LifeGame(BoxLayout):
 
         def wrap_on_press_rate(rate):
             def on_press_rate(instance):
+                """Callback for rate buttons"""
                 root = instance.parent.parent
                 root.update_rate = 60 // rate
             return on_press_rate
         self.grid = GridLayout(cols=len(self.field[0]), rows=len(self.field))
         for i, line in enumerate(self.field):
             for j, cell in enumerate(line):
-                btn = ToggleButton(background_color=blue,
-                                   background_normal='',
+                btn = ToggleButton(background_color=blue, background_normal='',
                                    on_press=on_press_field_cell)
                 if cell == 1:
-                    btn = ToggleButton(background_normal='',
-                                       background_color=blue,
-                                       state='down',
+                    btn = ToggleButton(background_normal='', background_color=blue, state='down',
                                        on_press=on_press_field_cell)
                 btn.coordinates = (i, j)
                 self.grid.add_widget(btn)
         controls = BoxLayout(orientation='vertical', size_hint=(.25, 1))
-        start_btn = ToggleButton(text='[b][color=2f4f4f]start[/color][/b]',
-                                 markup=True,
-                                 background_color=light_sky,
-                                 background_normal='',
-                                 group="game",
-                                 on_press=on_press_start, padding=(2, 2))
+        start_btn = ToggleButton(text='[b][color=2f4f4f]start[/color][/b]', markup=True,
+                                 background_color=light_sky, background_normal='',
+                                 group="game", on_press=on_press_start, padding=(2, 2))
         pause_btn = ToggleButton(text='[b][color=2f4f4f]pause[/color][/b]', markup=True,
-                                 background_color=light_sky,
-                                 background_normal='',
-                                 group="game",
+                                 background_color=light_sky, background_normal='', group="game",
                                  on_press=on_press_pause)
         rate_label = Label(text='[b][color=234f4f]Rate:[/color][/b]', markup=True,
                            size_hint=(1, 0.5))
         controls.add_widget(rate_label)
         for i in range(4):
             rate_btn = ToggleButton(text="[color=234f4f]{}x[/color]".format(2**i), group="rate",
-                                    markup=True,
-                                    background_color=light_sky,
-                                    background_normal='',
-                                    size_hint=(1, 0.5),
-                                    on_press=wrap_on_press_rate(2**i))
+                                    markup=True, background_color=light_sky, background_normal='',
+                                    size_hint=(1, 0.5), on_press=wrap_on_press_rate(2**i))
             if i == 0:
                 rate_btn.state = 'down'
             controls.add_widget(rate_btn)
@@ -84,6 +77,7 @@ class LifeGame(BoxLayout):
         self.add_widget(controls)
 
     def run_iteration(self):
+        """Computes one iteration of the game."""
         height = len(self.field)
         width = len(self.field[0])
         new_field = []
@@ -95,6 +89,7 @@ class LifeGame(BoxLayout):
         self.field = new_field
 
     def update_cell(self, i, j):
+        """ Computes cell (i,j) state for the next iteration."""
         new_cell = 0
         num_of_neighbours = self.num_of_neighbours(i, j)
         if self.field[i][j]:
@@ -104,6 +99,8 @@ class LifeGame(BoxLayout):
         return new_cell
 
     def num_of_neighbours(self, i, j):
+        """ Computes number of neighbours for cell
+            with coordinates (i,j)."""
         height = len(self.field)
         width = len(self.field[0])
         sum_ = 0
@@ -121,6 +118,7 @@ class LifeGame(BoxLayout):
         return sum_
 
     def update_grid(self, dt):
+        """ Computes one iteration of the game and updates UI every self.update_rate frames"""
         if not self.pause_pressed and self.updates_called % self.update_rate == 0:
             self.run_iteration()
             # self.print_field()
@@ -137,6 +135,9 @@ class LifeGame(BoxLayout):
         self.updates_called += 1
 
     def print_field(self):
+        """ Utility function, prints internal field
+            to terminal. Useful for debugging.
+        """
         for line in self.field:
             print()
             for cell in line:
